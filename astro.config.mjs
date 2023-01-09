3520d3223e9241c0cdb50f174c4a3f12af1f0dca
Christian Chiarulli <chris.machine@pm.me>
2023-01-08 20:03:28 -0500

    import static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
