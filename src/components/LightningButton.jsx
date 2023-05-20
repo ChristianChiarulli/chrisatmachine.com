b7e8722c99cf3d12dd801bf86527e741df72c619
Christian Chiarulli <chris.machine@pm.me>
2023-05-20 14:43:41 -0400

    feat: much better lightning tip button

--- src/components/LightningButton.jsx
+++ src/components/LightningButton.jsx
@@ -1,65 +1,29 @@
-// import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import { useEffect, useState } from "react";
-import { FaBeer } from "react-icons/fa";
+import { useStore } from "@nanostores/react";
+import { connected } from "../store/lightning";
+import { useEffect } from "react";
+
 import { BsLightningChargeFill } from "react-icons/bs";
 
-const LightningButton = () => {
-  const [connected, setConnected] = useState(false);
-  const [render, setRender] = useState(false);
-
-  // Will run once everytime a user connects to the dapp
-  useEffect(() => {
-    setTimeout(() => {
-      setRender(true);
-    }, 70);
-
-    // const getConnectedSession = async () => {
-    const connected = sessionStorage.getItem("connected");
-
-    // }
-
-    const getConnected = async (connected) => {
-      // console.log("CONNECTED:", connected);
-      let enabled = false;
-      if (connected === "true") {
-        enabled = await window.webln.enable();
-        setConnected(true);
-      }
-      return enabled;
-    };
-
-    getConnected(connected);
-
-    if (!getConnected) {
-      const connected2 = sessionStorage.getItem("connected");
-
-      setTimeout(() => {
-        setRender(true);
-      }, 300);
-
-      getConnected(connected2);
-    }
-
-    console.log("connected", connected);
-  }, []);
-
-  function waitForLocalStorage(key, cb, timer) {
-    if (!sessionStorage.getItem(key))
-      return (timer = setTimeout(waitForLocalStorage.bind(null, key), 100));
-    clearTimeout(timer);
-    if (typeof cb !== "function") return localStorage.getItem(key);
-    return cb(localStorage.getItem(key));
-  }
+export default function LightningButton() {
+  const $connected = useStore(connected);
 
   const connectHandler = async () => {
-    if (typeof window.webln !== "undefined") {
+    try {
       const enabled = await window.webln.enable();
-
-      sessionStorage.setItem("connected", "true");
+      if (enabled) {
+        connected.set("true");
+      }
+    } catch (e) {
+      console.log("disconnected", e);
+      connected.set("false");
     }
-    console.log("connected");
-    setConnected(true);
   };
+
+  useEffect(() => {
+    if ($connected === "true") {
+      connectHandler();
+    }
+  }, []);
 
   const tipHandler = async () => {
     document.getElementById("myPopup").style.display = "flex";
@@ -67,42 +31,17 @@ const LightningButton = () => {
 
   return (
     <div>
-      {render ? (
-        <div>
-          {connected ? (
-            <button className="connect-button" onClick={tipHandler}>
-              <BsLightningChargeFill
-                color={"#111"}
-                size="17"
-                style={{ marginRight: ".4rem" }}
-              />
-              Tip
-            </button>
-          ) : (
-            <button className="tip-button" onClick={connectHandler}>
-              <BsLightningChargeFill
-                color={"#111"}
-                size="17"
-                style={{ marginRight: ".4rem" }}
-              />
-              Tip
-            </button>
-          )}
-        </div>
-      ) : (
-        <div>
-          <button className="connect-button" onClick={tipHandler}>
-            <BsLightningChargeFill
-              color={"#111"}
-              size="17"
-              style={{ marginRight: ".4rem" }}
-            />
-            Tip
-          </button>
-        </div>
-      )}
+      <button
+        className="connect-button"
+        onClick={$connected === "true" ? tipHandler : connectHandler}
+      >
+        <BsLightningChargeFill
+          color={"#111"}
+          size="17"
+          style={{ marginRight: ".4rem" }}
+        />
+        tip
+      </button>
     </div>
   );
-};
-
-export default LightningButton;
+}
---
+++ src/store/lightning.js
@@ -0,0 +1,4 @@
+import { persistentAtom } from "@nanostores/persistent";
+
+export const connected = persistentAtom('chrisatmachine_connected', 'false');
+
